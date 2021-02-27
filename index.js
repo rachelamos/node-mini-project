eb5f7244c8d15d00fc0819079ed6664a74cdecb4
Rachel Amos <rachelamos35@gmail.com>
2021-02-27 13:48:22 -0600

    adding .then functionality

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ inquirer
       message: 'What is your name?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    },
-    {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
+      type: 'input',
+      message: "What's your story",
+      name: 'bio',
+    }
+  ])
+  
+    .then((data) => {
+      const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+  
+      fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+      );
+    });
